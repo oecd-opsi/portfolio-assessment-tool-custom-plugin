af30a82a543a0da0dafa25a5478bf357e2b4ec7c
conlaccento <antonio@blackstudio.it>
2020-07-23 19:47:41 +0200

    results page style

--- portfolio-assessment-tool-custom-plugin.php
+++ portfolio-assessment-tool-custom-plugin.php
@@ -368,7 +368,7 @@ function bp_pat_list() {
 					if ( $post_status_obj->label == 'Published' ) {
 						echo 'Complete';
 					} else {
-						echo $post_status_obj->label;						
+						echo $post_status_obj->label;
 					}
 					?>
 				</td>
@@ -496,4 +496,78 @@ function bs_pat_form_template_redirect() {
 		wp_redirect( wp_login_url( get_permalink( get_page_by_path( 'portfolio-exploration' ) ) ) );
 		die;
 	}
+}
+
+// Helper function: transform hsl to hex color
+function ColorHSLToRGB($h, $s, $l){
+
+	$h /= 360;
+  $s /= 100;
+  $l /= 100;
+
+  $r = $l;
+  $g = $l;
+  $b = $l;
+  $v = ($l <= 0.5) ? ($l * (1.0 + $s)) : ($l + $s - $l * $s);
+  if ($v > 0){
+    $m;
+    $sv;
+    $sextant;
+    $fract;
+    $vsf;
+    $mid1;
+    $mid2;
+
+    $m = $l + $l - $v;
+    $sv = ($v - $m ) / $v;
+    $h *= 6.0;
+    $sextant = floor($h);
+    $fract = $h - $sextant;
+    $vsf = $v * $sv * $fract;
+    $mid1 = $m + $vsf;
+    $mid2 = $v - $vsf;
+
+    switch ($sextant)
+    {
+      case 0:
+        $r = $v;
+        $g = $mid1;
+        $b = $m;
+        break;
+      case 1:
+        $r = $mid2;
+        $g = $v;
+        $b = $m;
+        break;
+      case 2:
+        $r = $m;
+        $g = $v;
+        $b = $mid1;
+        break;
+      case 3:
+        $r = $m;
+        $g = $mid2;
+        $b = $v;
+        break;
+      case 4:
+        $r = $mid1;
+        $g = $m;
+        $b = $v;
+        break;
+      case 5:
+        $r = $v;
+        $g = $m;
+        $b = $mid2;
+        break;
+    }
+  }
+  // return array('r' => $r * 255.0, 'g' => $g * 255.0, 'b' => $b * 255.0);
+	$r = round($r * 255, 0);
+  $g = round($g * 255, 0);
+  $b = round($b * 255, 0);
+
+  $r = ($r < 15)? '0' . dechex($r) : dechex($r);
+  $g = ($g < 15)? '0' . dechex($g) : dechex($g);
+  $b = ($b < 15)? '0' . dechex($b) : dechex($b);
+	return "#$r$g$b";
 }
--- single-pat_submission.php
+++ single-pat_submission.php
@@ -109,6 +109,12 @@
   $ant_percentage = round( $ant / $max * 100 );
   $ada_percentage = round( $ada / $max * 100 );
 
+  // Transform percentage into color
+  $enh_color = ColorHSLToRGB( 288, 75, $enh_percentage );
+  $mis_color = ColorHSLToRGB( 288, 75, $mis_percentage );
+  $ant_color = ColorHSLToRGB( 288, 75, $ant_percentage );
+  $ada_color = ColorHSLToRGB( 288, 75, $ada_percentage );
+
   //* Portfolio Tendency Group
   $portfolio_tendency_group = '';
   $portfolio_tendency_group_text = '';
@@ -287,8 +293,8 @@
       <!--<li><a href="#combined-results">Combined Results</a></li>-->
       <li><a href="#download-and-share">Download and Share Results</a></li>
       <li><a href="#interpretation">Interpretationn and Next Steps</a></li>
-      <li><a href="#">Share icon</a></li>
-      <li><a href="#">Start again</a></li>
+      <li class="nav-share-item"><a href="#"><span>Share icon</span></a></li>
+      <li class="nav-start-again-item"><a href="#">Start again</a></li>
     </ul>
   ';
 
@@ -299,123 +305,142 @@
 
         <div class="entry-content">
 
-
-          <div class="pat-results-hero"><h1>Portfolio Exploration Tool Results</h1></div>
+          <div class="col-md-12 pat-results-hero">
+          	<div class="pat-form-title-wrapper screen-reader-text">
+              <h1>Portfolio Exploration Tool Results</h1>
+          	</div>
+          	<div class="single_img_wrap pat-form-banner">
+          		<img src="<?php echo plugin_dir_url( __FILE__ ) ?>assets/images/pat-results-hero.jpg" class="attachment-blog size-blog wp-post-image" alt="Portfolio Exploration Results">
+          	</div>
+          </div>
 
           <div class="pat-results-top-nav"><?php echo $nav_menu ?></div>
 
-          <main class="pat-results-main-content">
+          <div class="col-md-12">
 
-            <aside class="pat-results-side"><?php echo $nav_menu ?></aside>
+            <main class="pat-results-main-content">
 
-            <section id="introduction">
+              <aside class="pat-results-side"><?php echo $nav_menu ?></aside>
 
-              <div class="pat-results-content">
+              <section id="introduction">
 
-                <h2>Innovation portfolio balance of <?php echo $organisation ?></h2>
-                <p class="subtitle">Based on exploration by <?php echo $name ?> on <?php echo $submission_date ?></p>
+                <div class="pat-results-content">
 
-                <p class="disclaimer"><?php echo get_field( 'disclaimer', 'option' ) ?></p>
+                  <div class="pat-results-main-title">
+                    <h2>Innovation portfolio balance of <?php echo $organisation ?></h2>
+                    <p class="subtitle">Based on exploration by <?php echo $name ?> on <?php echo $submission_date ?></p>
+                  </div>
 
-                <h3>Your organisational portfolio tends to <?php echo $portfolio_tendency_group ?> innovation</h3>
+                  <div class="disclaimer"><?php echo get_field( 'disclaimer_text', 'option' ) ?></div>
 
-              </div>
+                  <!-- Diamond 1 -->
+                  <div class="diamond diamond-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="803" height="445" viewBox="0 0 803 445"><defs><style>.a{fill:none;stroke:#fff;stroke-width:3px;}.b{fill:<?php echo $enh_color ?>;}.c{fill:<?php echo $mis_color ?>;}.d{fill:<?php echo $ada_color ?>;}.e{fill:<?php echo $ant_color ?>;}.f{fill:#605f5f;font-size:12px;}.f,.g{font-family:Roboto-Medium, Roboto;font-weight:500;}.g{fill:#fff;font-size:33px;}.h{filter:url(#i);}.i{filter:url(#g);}.j{filter:url(#e);}.k{filter:url(#c);}.l{filter:url(#a);}</style><filter id="a" x="125.265" y="25.211" width="572.267" height="381.245" filterUnits="userSpaceOnUse"><feOffset dy="3" input="SourceAlpha"/><feGaussianBlur stdDeviation="3" result="b"/><feFlood flood-opacity="0.161"/><feComposite operator="in" in2="b"/><feComposite in="SourceGraphic"/></filter><filter id="c" x="186.614" y="149.628" width="214.802" height="135.775" filterUnits="userSpaceOnUse"><feOffset dy="3" input="SourceAlpha"/><feGaussianBlur stdDeviation="1.5" result="d"/><feFlood flood-opacity="0.349"/><feComposite operator="in" in2="d"/><feComposite in="SourceGraphic"/></filter><filter id="e" x="305.324" y="71.092" width="214.802" height="135.775" filterUnits="userSpaceOnUse"><feOffset dy="3" input="SourceAlpha"/><feGaussianBlur stdDeviation="1.5" result="f"/><feFlood flood-opacity="0.349"/><feComposite operator="in" in2="f"/><feComposite in="SourceGraphic"/></filter><filter id="g" x="305.324" y="227.27" width="214.802" height="135.775" filterUnits="userSpaceOnUse"><feOffset dy="3" input="SourceAlpha"/><feGaussianBlur stdDeviation="1.5" result="h"/><feFlood flood-opacity="0.349"/><feComposite operator="in" in2="h"/><feComposite in="SourceGraphic"/></filter><filter id="i" x="424.927" y="149.628" width="214.802" height="135.775" filterUnits="userSpaceOnUse"><feOffset dy="3" input="SourceAlpha"/><feGaussianBlur stdDeviation="1.5" result="j"/><feFlood flood-opacity="0.349"/><feComposite operator="in" in2="j"/><feComposite in="SourceGraphic"/></filter></defs><g transform="translate(-490 -1096.295)"><g class="l" transform="matrix(1, 0, 0, 1, 490, 1096.29)"><path class="a" d="M2580.007,3511.292l274.127-178.347L3128.8,3511.292,2854.135,3692.6Z" transform="translate(-2443.01 -3299.95)"/></g><g class="k" transform="matrix(1, 0, 0, 1, 490, 1096.29)"><path class="b" d="M749.043,1424.834l-102.9,62.941-102.9-62.941L646.142,1361Z" transform="translate(-352.13 -1209.87)"/></g><g transform="translate(799.824 1168.887)"><g class="j" transform="matrix(1, 0, 0, 1, -309.82, -72.59)"><path class="c" d="M871.043,1347.834l-102.9,62.941-102.9-62.941L768.142,1284Z" transform="translate(-355.42 -1211.41)"/></g></g><g class="i" transform="matrix(1, 0, 0, 1, 490, 1096.29)"><path class="d" d="M871.043,1500.834l-102.9,62.941-102.9-62.941L768.142,1437Z" transform="translate(-355.42 -1208.23)"/></g><g class="h" transform="matrix(1, 0, 0, 1, 490, 1096.29)"><path class="e" d="M994.043,1424.834l-102.9,62.941-102.9-62.941L891.142,1361Z" transform="translate(-358.81 -1209.87)"/></g><text class="f" transform="translate(1245 1308.295)"><tspan x="-39.574" y="0">UNCERTAINTY</tspan><tspan x="-47.317" y="14">Exploring/Radical</tspan></text><text class="f" transform="translate(897.224 1524.295)"><tspan x="-35.707" y="0">UNDIRECTED</tspan><tspan x="-62.745" y="14">Responding/Bottom Up</tspan></text><text class="f" transform="translate(907.224 1107.295)"><tspan x="-27.536" y="0">DIRECTED</tspan><tspan x="-53.484" y="14">Shaping / Top Down</tspan></text><text class="f" transform="translate(553 1301.295)"><tspan x="-31.403" y="0">CERTAINTY</tspan><tspan x="-62.396" y="14">Exploiting/ Incremental</tspan></text><text class="g" transform="translate(902.528 1240)"><tspan x="-30.873" y="0"><?php echo $mis_percentage ?>%</tspan></text><text class="g" transform="translate(902.528 1399)"><tspan x="-30.873" y="0"><?php echo $ada_percentage ?>%</tspan></text><text class="g" transform="translate(1021.528 1322)"><tspan x="-30.873" y="0"><?php echo $ant_percentage ?>%</tspan></text><text class="g" transform="translate(783.528 1316)"><tspan x="-30.873" y="0"><?php echo $enh_percentage ?>%</tspan></text></g></svg>
+                  </div>
 
-            </section>
-
-            <section id="organisational-portfolio-balance" class="pat-results-fullwidth-section">
-
-              <div class="pat-results-side">
-                <p><?php echo $portfolio_tendency_group ?> innovation</p>
-                <?php // TODO: diamonds with tendency highlighted, one for each facet ?>
-                <?php // TODO: diamond with all percentage, showing when tendency group text ?>
-              </div>
-
-              <div class="pat-results-content">
-
-                <h2>Innovation portfolio balance of <?php echo $organisation ?></h2>
-
-                <?php
-                echo '<p>Enhancement: ' . $enh_percentage . '%</p>';
-                echo '<p>Mission: ' . $mis_percentage . '%</p>';
-                echo '<p>Anticipatory: ' . $ant_percentage . '%</p>';
-                echo '<p>Adaptive: ' . $ada_percentage. '%</p>';
-                 ?>
-
-                <div class="enhancement-inno-section">
-                  <?php // TODO:diamond with percentage  ?>
-                  <?php the_field( 'enhancemnet_oriented_innovation_text', 'option' ); ?>
-                </div>
-
-                <div class="mission-inno-section">
-                  <?php // TODO:diamond with percentage  ?>
-                  <?php the_field( 'mission_oriented_innovation_text', 'option' ); ?>
-                </div>
-
-                <div class="adaptive-inno-section">
-                  <?php // TODO:diamond with percentage  ?>
-                  <?php the_field( 'adaptive_oriented_innovation_text', 'option' ); ?>
-                </div>
-
-                <div class="anticipatory-inno-section">
-                  <?php // TODO:diamond with percentage  ?>
-                  <?php the_field( 'anticipatory_oriented_innovation_text_', 'option' ); ?>
-                </div>
-
-                <div class="tendency-group">
-
-                  <p>This organisation’sportfolio tends to be strong in <?php echo $portfolio_tendency_group ?>-oriented innovation.</p>
-
-                  <?php echo $portfolio_tendency_group_text ?>
+                  <h3>Your organisational portfolio tends to <?php echo $portfolio_tendency_group ?> innovation</h3>
 
                 </div>
 
-              </div>
+              </section>
 
-            </section>
+              <section id="organisational-portfolio-balance" class="pat-results-fullwidth-section">
 
-            <section id="portfolio-management-capability" class="pat-results-fullwidth-section">
+                <div class="pat-results-side">
+                  <p><?php echo $portfolio_tendency_group ?> innovation</p>
+                  <?php // TODO: diamonds with tendency highlighted, one for each facet ?>
+                  <?php // TODO: diamond with all percentage, showing when tendency group text ?>
+                </div>
 
-              <div class="pat-results-side">
-                <p>Portfolio balance</p>
-                <?php // TODO: diamond with all percentage ?>
-                <p>Portfolio management Capability</p>
-                <?php // TODO: low/med/high graph ?>
-              </div>
+                <div class="pat-results-content">
 
-              <div class="pat-results-content">
+                  <h2 class="section-title">Innovation portfolio balance of <strong><?php echo $organisation ?></strong></h2>
 
-                <p>This organisation’s tends to have <?php echo $level ?> portfolio management capablity</p>
+                  <?php
+                  echo '<p>Enhancement: ' . $enh_percentage . '%</p>';
+                  echo '<p>Mission: ' . $mis_percentage . '%</p>';
+                  echo '<p>Anticipatory: ' . $ant_percentage . '%</p>';
+                  echo '<p>Adaptive: ' . $ada_percentage. '%</p>';
+                   ?>
 
-                <?php // TODO: low/med/high graph ?>
+                  <div class="enhancement-inno-section">
+                    <!-- diamond with enhancement percentage   -->
+                    ?>
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="548.793" height="343.681" viewBox="0 0 548.793 343.681"><defs><style>.a{fill:#e6e8f0;}.b{fill:<?php echo $enh_color ?>;}.c,.e,.f{opacity:0.201;}.d{fill:<?php echo $mis_color ?>;}.e{fill:<?php echo $ada_color ?>;}.f{fill:<?php echo $ant_color ?>;}.g,.h{fill:#fff;font-family:Roboto-Medium, Roboto;font-weight:500;}.g{font-size:20px;}.h{font-size:33px;}.i{filter:url(#g);}.j{filter:url(#e);}.k{filter:url(#c);}.l{filter:url(#a);}</style><filter id="a" x="49.614" y="105.65" width="214.802" height="135.775" filterUnits="userSpaceOnUse"><feOffset dy="3" input="SourceAlpha"/><feGaussianBlur stdDeviation="1.5" result="b"/><feFlood flood-opacity="0.349"/><feComposite operator="in" in2="b"/><feComposite in="SourceGraphic"/></filter><filter id="c" x="168.324" y="27.115" width="214.802" height="135.775" filterUnits="userSpaceOnUse"><feOffset dy="3" input="SourceAlpha"/><feGaussianBlur stdDeviation="1.5" result="d"/><feFlood flood-opacity="0.349"/><feComposite operator="in" in2="d"/><feComposite in="SourceGraphic"/></filter><filter id="e" x="168.324" y="183.293" width="214.802" height="135.775" filterUnits="userSpaceOnUse"><feOffset dy="3" input="SourceAlpha"/><feGaussianBlur stdDeviation="1.5" result="f"/><feFlood flood-opacity="0.349"/><feComposite operator="in" in2="f"/><feComposite in="SourceGraphic"/></filter><filter id="g" x="287.927" y="105.65" width="214.802" height="135.775" filterUnits="userSpaceOnUse"><feOffset dy="3" input="SourceAlpha"/><feGaussianBlur stdDeviation="1.5" result="h"/><feFlood flood-opacity="0.349"/><feComposite operator="in" in2="h"/><feComposite in="SourceGraphic"/></filter></defs><g transform="translate(-627 -1980.272)"><path class="a" d="M2580.007,3503.369l274.127-170.424L3128.8,3503.369l-274.666,173.257Z" transform="translate(-1953.007 -1352.673)"/><g class="l" transform="matrix(1, 0, 0, 1, 627, 1980.27)"><path class="b" d="M749.043,1424.834l-102.9,62.941-102.9-62.941L646.142,1361Z" transform="translate(-489.13 -1253.85)"/></g><g class="c" transform="translate(799.824 2008.887)"><g class="k" transform="matrix(1, 0, 0, 1, -172.82, -28.62)"><path class="d" d="M871.043,1347.834l-102.9,62.941-102.9-62.941L768.142,1284Z" transform="translate(-492.42 -1255.38)"/></g></g><g class="j" transform="matrix(1, 0, 0, 1, 627, 1980.27)"><path class="e" d="M871.043,1500.834l-102.9,62.941-102.9-62.941L768.142,1437Z" transform="translate(-492.42 -1252.21)"/></g><g class="i" transform="matrix(1, 0, 0, 1, 627, 1980.27)"><path class="f" d="M994.043,1424.834l-102.9,62.941-102.9-62.941L891.142,1361Z" transform="translate(-495.81 -1253.85)"/></g><text class="g" transform="translate(902.528 2080)"><tspan x="-40.474" y="0">MISSION</tspan></text><text class="g" transform="translate(898.528 2235)"><tspan x="-47.148" y="0">ADAPTIVE</tspan></text><text class="g" transform="translate(1022.528 2155)"><tspan x="-68.384" y="0">ANTICIPATORY</tspan></text><text class="h" transform="translate(790 2162)"><tspan x="-30.873" y="0"><?php echo $enh_percentage ?>%</tspan></text></g></svg>
+                    <?php the_field( 'enhancemnet_oriented_innovation_text', 'option' ); ?>
+                  </div>
 
-                <?php the_field( 'portfolio_management_capability_text', 'option' ); ?>
+                  <div class="mission-inno-section">
+                    <?php // TODO:diamond with percentage  ?>
+                    <?php the_field( 'mission_oriented_innovation_text', 'option' ); ?>
+                  </div>
 
-              </div>
+                  <div class="adaptive-inno-section">
+                    <?php // TODO:diamond with percentage  ?>
+                    <?php the_field( 'adaptive_oriented_innovation_text', 'option' ); ?>
+                  </div>
 
-            </section>
+                  <div class="anticipatory-inno-section">
+                    <?php // TODO:diamond with percentage  ?>
+                    <?php the_field( 'anticipatory_oriented_innovation_text_', 'option' ); ?>
+                  </div>
 
-            <section id="download-and-share">
-              <div class="pat-results-content">
-                <h2>Download and share results</h2>
-                <?php the_field( 'download_and_share_results_text', 'option' ) ?>
-                <button class="download-pdf">Download PDF</button>
-                <button class="download-csv">Download CSV</button>
-              </div>
-            </section>
+                  <div class="tendency-group">
 
-            <section id="#interpretation">
-              <div class="pat-results-content">
-                <h2>How you might use these results</h2>
-                <?php the_field( 'how_you_might_use_these_result_text', 'option' ) ?>
-                <h2>Save, share, and continue to next steps</h2>
-                <?php the_field( 'save_share_and_continue_to_next_steps_text', 'option' ) ?>
-                <h2>Contact OPSI for help with interpretation</h2>
-                <?php the_field( ' contact_OPSI_for_help_with_interpretation_text', 'option' ) ?>
-              </div>
-            </section>
+                    <p>This organisation’sportfolio tends to be strong in <?php echo $portfolio_tendency_group ?>-oriented innovation.</p>
 
-          </main>
+                    <?php echo $portfolio_tendency_group_text ?>
+
+                  </div>
+
+                </div>
+
+              </section>
+
+              <section id="portfolio-management-capability" class="pat-results-fullwidth-section">
+
+                <div class="pat-results-side">
+                  <p>Portfolio balance</p>
+                  <?php // TODO: diamond with all percentage ?>
+                  <p>Portfolio management Capability</p>
+                  <?php // TODO: low/med/high graph ?>
+                </div>
+
+                <div class="pat-results-content">
+
+                  <p>This organisation’s tends to have <?php echo $level ?> portfolio management capablity</p>
+
+                  <?php // TODO: low/med/high graph ?>
+
+                  <?php the_field( 'portfolio_management_capability_text', 'option' ); ?>
+
+                </div>
+
+              </section>
+
+              <section id="download-and-share">
+                <div class="pat-results-content">
+                  <h2>Download and share results</h2>
+                  <?php the_field( 'download_and_share_results_text', 'option' ) ?>
+                  <button class="download-pdf">Download PDF</button>
+                  <button class="download-csv">Download CSV</button>
+                </div>
+              </section>
+
+              <section id="#interpretation">
+                <div class="pat-results-content">
+                  <h2>How you might use these results</h2>
+                  <?php the_field( 'how_you_might_use_these_result_text', 'option' ) ?>
+                  <h2>Save, share, and continue to next steps</h2>
+                  <?php the_field( 'save_share_and_continue_to_next_steps_text', 'option' ) ?>
+                  <h2>Contact OPSI for help with interpretation</h2>
+                  <?php the_field( ' contact_OPSI_for_help_with_interpretation_text', 'option' ) ?>
+                </div>
+              </section>
+
+            </main>
+
+          </div>
 
         </div><!-- end entry-content -->
 
